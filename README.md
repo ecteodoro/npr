cc3d4754bb8e484f4ff61f1ca6c71d2d08bb28bc
Ecil Teodoro <ecil.teodoro@gmail.com>
2013-10-04 16:08:17 -0300

    refactored code, added javadoc comments and added sample output files

--- README.md
+++ README.md
@@ -2,3 +2,48 @@
 
 
 This is a very simple Non Personalized Recommender created as an assignment for the course "Recommender Systems" at Coursera.
+
+## Overview of the assignment
+
+This assignment will explore non-personalized recommendations. You will be given a program stub and a data set in .csv format. You will use these to write a program that makes basic, non-personalized recommendations.
+
+**Ratings Data** — This is a comma-separated values file, with the user number, movie ID, and rating, in that order.
+
+**Movies List** — Decoding the movie title is not required for this assignment, but are you curious which movie is which in the ratings file? Use this file to find out!
+
+**Users List** — Decoding the unique user id is not required for this assignment, but use this file to find your user number using the unique identifier that was provided to you after rating the movies.
+
+## Notes
+
+This assignment requires you to write the code needed to parse the ratings file. It is up to you how you do this (including whether you skip ahead and use LensKit data structures or simply build your own matrix). It is particularly important to make sure you can distinguish between rated and non-rated cells in your matrix.
+
+## Deliverables
+
+There are 2 deliverables for this assignment. Each deliverable represents a different analysis of the data provided to you. For each deliverable, you will submit a list of the top 5 movies that occur with movies A, B, and C; where A, B, and C will be uniquely assigned to you. Do this for each of the two association formulas described in class (remember, your movie is x, and you are looking for the other movies y that maximize the formula values):
+
+    Simple: (x and y) / x
+    Advanced: ((x and y) / x) / ((!x and y) / !x)
+
+## Output Format
+
+For each formula, your output should be as CSV file (a file of comma-separated values) defined as follows:
+
+Each file will have three rows (one for each movie you're computing associations for). Each row will have the movie ID of the movie assigned to you, followed by five pairs of "movie ID,predicted-score", from first to last, showing the top-five associated movies using that formula.
+
+Note: You will be graded on both choosing the right movies and getting correct scores (rounded to the hundredths place), therefore you should provide at least two decimal places precision on your predicted scores.
+
+## Examples
+
+Suppose that you were assigned movie IDs 11, 121, and 8587. Your submission for the first part (simple formula) would be:
+
+    11,603,0.96,1892,0.94,1891,0.94,120,0.93,1894,0.93
+    121,120,0.95,122,0.95,603,0.94,597,0.89,604,0.88
+    8587,603,0.92,597,0.90,607,0.87,120,0.86,13,0.86
+
+...and your submission for the second part (advanced formula) would be:
+
+    11,1891,5.69,1892,5.65,243,5.00,1894,4.72,2164,4.11
+    121,122,4.74,120,3.82,2164,3.40,243,3.26,1894,3.22
+    8587,10020,4.18,812,4.03,7443,2.63,9331,2.46,786,2.39
+
+Note that with rounding, some entries will appear to tie. Be sure to preserve the order of the output from the original algorithm.
--- src/NonPersonalizedRecommender.java
+++ src/NonPersonalizedRecommender.java
@@ -9,11 +9,12 @@ import java.util.*;
 import static java.util.Collections.sort;
 
 /**
- * Created with IntelliJ IDEA.
+ * A Simple Non Personalized Recommender
+ *
  * User: ecil
  * Date: 10/2/13
  * Time: 9:49 AM
- * To change this template use File | Settings | File Templates.
+ *
  */
 public class NonPersonalizedRecommender {
 
@@ -21,6 +22,13 @@ public class NonPersonalizedRecommender {
     private static final String USERS_FILE = "data/recsys_data_users.csv";
     private static final String RATINGS_FILE = "data/recsys_data_ratings.csv";
 
+    /**
+     * This inner class is a simple data structure to store
+     * a movieId and its associated score.
+     * Most often used as a collection of these objects.
+     * Its Comparable feature allows a collection of these objects
+     * to be sorted (in descending order, to get the top scored movies).
+     */
     private class MovieScore implements Comparable<MovieScore> {
 
         public int movieId;
@@ -32,7 +40,7 @@ public class NonPersonalizedRecommender {
         }
 
         public int compareTo(MovieScore o) {
-            // returned values are inverse to sort in reverse order
+            // returned values are inverse to sort in descending order
             if (score > o.score)
                 return -1;
             else if (score < o.score)
@@ -42,11 +50,10 @@ public class NonPersonalizedRecommender {
         }
     }
 
-    /*
-    Returns a HashMap in this format:
-    {title_code: title_name,
-     title_code: title_name,
-     ...}
+    /**
+     * Loads movies from a CSV data file.
+     *
+     * @return a HashMap in this format: {title_code: title_name, title_code: title_name, ...}
      */
     private Map<String, String> loadMovies() {
         Map<String, String> movies = new HashMap<String, String>();
@@ -64,11 +71,10 @@ public class NonPersonalizedRecommender {
         return movies;
     }
 
-    /*
-    Returns a HashMap in this format:
-    {user_code: user_name,
-     user_code: user_name,
-     ...}
+    /**
+     * Loads users from a CSV data file.
+     *
+     * @return a HashMap in this format: {user_code: user_name, user_code: user_name, ...}
      */
     private Map<String, String> loadUsers() {
         Map<String, String> users = new HashMap<String, String>();
@@ -87,17 +93,19 @@ public class NonPersonalizedRecommender {
     }
 
 
-    /*
-    Returns a HashMap in this format:
-    {title_code: {user_code: rating,
-                  user_code: rating,
-                  user_code: rating,
-                  ...},
-     title_code: {user_code: rating,
-                  user_code: rating,
-                  user_code: rating,
-                  ...},
-     ...}
+    /**
+     * Loads ratings from a CSV data file and returns a HashMap in this format:
+     * {movieID: {userID: rating,
+     *            userID: rating,
+     *            userID: rating,
+     *            ...},
+     *  movieID: {userID: rating,
+     *            userID: rating,
+     *            userID: rating,
+     *            ...},
+     *  ...}
+     *
+     *  @return a HashMap containing ratings
      */
     private Map<String, Map<String, Float>> loadRatings() {
         Map<String, Map<String, Float>> ratings = new HashMap<String, Map<String, Float>>();
@@ -126,6 +134,14 @@ public class NonPersonalizedRecommender {
         return ratings;
     }
 
+    /**
+     * Returns the number of elements that exist in both sets (intersection).
+     * Given set A and set B, returns the cardinality of (A & B).
+     *
+     * @param set1 the first set
+     * @param set2 the second set
+     * @return the number of common elements
+     */
     private int commonElementCount(Set<String> set1, Set<String> set2) {
         int count = 0;
         for (String element : set1) {
@@ -135,32 +151,15 @@ public class NonPersonalizedRecommender {
         return count;
     }
 
-    private MovieScore[] topMoviesSimpleAssociation(int movie, Map<String, Map<String, Float>> ratings, int count) {
-        MovieScore[] topMovies = new MovieScore[count];
-        int referenceMovieCount;
-        referenceMovieCount = ratings.get(Integer.toString(movie)).keySet().size();
-
-        Set<String> referenceUserSet = ratings.get(String.valueOf(movie)).keySet();
-
-        List<MovieScore> movieScoreList = new ArrayList<MovieScore>();
-        for (String associatedMovie : ratings.keySet()) {
-            int associatedMovieCode = Integer.parseInt(associatedMovie);
-            if (associatedMovieCode == movie)
-                continue;
-            Set<String> associatedUserSet = ratings.get(associatedMovie).keySet();
-
-            float simpleScore = commonElementCount(referenceUserSet, associatedUserSet) / (float) referenceMovieCount;
-            MovieScore ms = new MovieScore(associatedMovieCode, simpleScore);
-            movieScoreList.add(ms);
-
-        }
-        sort(movieScoreList);
-        for (int i = 0; i < count; i++) {
-            topMovies[i] = movieScoreList.get(i);//movieScoreList.get(movieScoreList.size() - i - 1);
-        }
-        return topMovies;
-    }
-
+    /**
+     * Given two sets, returns a third set that contains elements
+     * that are members of the first set and are not members of the second set.
+     * Given sets A and B, return C = (A \ B).
+     *
+     * @param set1 first set
+     * @param set2 second set
+     * @return the relative complement of the second set in the first set
+     */
     private Set<String> differenceSet(Set<String> set1, Set<String> set2) {
         Set<String> differenceSet = new HashSet<String>();
         for (String key : set1) {
@@ -171,42 +170,85 @@ public class NonPersonalizedRecommender {
         return differenceSet;
     }
 
+    /**
+     * Returns the top N movies associated with another movie, using a simple formula.
+     *
+     * @param movie the reference movie
+     * @param ratings the ratings matrix
+     * @param count how many top N movies must be associated and returned
+     * @return the top N movies
+     */
+    private MovieScore[] topMoviesSimpleAssociation(int movie, Map<String, Map<String, Float>> ratings, int count) {
+        MovieScore[] topMovies = calculateTopMovies(movie, ratings, count, null, false);
+        return topMovies;
+    }
+
+    /**
+     * Returns the top N movies associated with another movie, using an advanced formula.
+     *
+     * @param movie the reference movie
+     * @param ratings the ratings matrix
+     * @param count how many top N movies must be associated and returned
+     * @param users the list of all users who rated movies
+     * @return
+     */
     private MovieScore[] topMoviesAdvancedAssociation(int movie, Map<String, Map<String, Float>> ratings, int count, Map<String, String> users) {
+        MovieScore[] topMovies = calculateTopMovies(movie, ratings, count, users, true);
+        return topMovies;
+    }
+
+    /**
+     * Return a list of the top 5 movies that occur with a certain Movie A (reference movie).
+     *
+     * @param movie the reference movie
+     * @param ratings the ratings matrix
+     * @param count how many top movies will be recommended (in this case it is 5)
+     * @param users the list of users who rated movies
+     * @param isAdvancedMode calculate using advanced formula (true) or using simple formula (false)
+     * @return an array of {@link MovieScore}
+     */
+    private MovieScore[] calculateTopMovies(int movie, Map<String, Map<String, Float>> ratings, int count, Map<String, String> users, boolean isAdvancedMode) {
         MovieScore[] topMovies = new MovieScore[count];
-        int referenceMovieCount;
-        referenceMovieCount = ratings.get(Integer.toString(movie)).keySet().size();
-
+        // how many users rated the reference movie
+        int referenceMovieCount = ratings.get(Integer.toString(movie)).keySet().size();
+        // the list of users/rating that rated the reference movie
         Set<String> referenceUserSet = ratings.get(String.valueOf(movie)).keySet();
-
-        System.out.println("Ref: " + referenceUserSet.size());
-
-        Set<String> usersThatDidNotRateTheMovieSet = differenceSet(users.keySet(), referenceUserSet);
-        System.out.println("Diff: " + usersThatDidNotRateTheMovieSet.size());
-
-        System.out.println("Total: " + (referenceUserSet.size() + usersThatDidNotRateTheMovieSet.size()) );
+        Set<String> usersThatDidNotRateTheMovieSet = null;
+        if (isAdvancedMode)
+            usersThatDidNotRateTheMovieSet = differenceSet(users.keySet(), referenceUserSet);
         List<MovieScore> movieScoreList = new ArrayList<MovieScore>();
+        // reads the users ratings associated with other movies and calculate an association score
         for (String associatedMovie : ratings.keySet()) {
             int associatedMovieCode = Integer.parseInt(associatedMovie);
             if (associatedMovieCode == movie)
-                continue;
+                continue; // skip when movie is the reference movie
             Set<String> associatedUserSet = ratings.get(associatedMovie).keySet();
-
-            float usersThatDidNotRateTheMovieCount = (float)usersThatDidNotRateTheMovieSet.size();
-            float simpleScore = commonElementCount(referenceUserSet, associatedUserSet) / (float) referenceMovieCount;
-
-            float advancedScore = simpleScore / (commonElementCount(usersThatDidNotRateTheMovieSet, associatedUserSet) / usersThatDidNotRateTheMovieCount);
-
-            MovieScore ms = new MovieScore(associatedMovieCode, advancedScore);
+            float score = commonElementCount(referenceUserSet, associatedUserSet) / (float) referenceMovieCount;
+            if (isAdvancedMode) {
+                float usersThatDidNotRateTheMovieCount = (float)usersThatDidNotRateTheMovieSet.size();
+                score = score / (commonElementCount(usersThatDidNotRateTheMovieSet, associatedUserSet) / usersThatDidNotRateTheMovieCount);
+            }
+            MovieScore ms = new MovieScore(associatedMovieCode, score);
             movieScoreList.add(ms);
-
         }
+        // sort the score list in descending order and pick the top N movies
         sort(movieScoreList);
         for (int i = 0; i < count; i++) {
-            topMovies[i] = movieScoreList.get(i);//movieScoreList.get(movieScoreList.size() - i - 1);
+            topMovies[i] = movieScoreList.get(i);
         }
         return topMovies;
     }
 
+    /**
+     * Format a row for CSV file with recommendations.
+     * Each row will have the movie ID of the movie assigned to you,
+     * followed by five pairs of "movie ID,predicted-score", from first to last,
+     * showing the top-five associated movies using that formula.
+     * Scores must have at least 2 decimal places.
+     *
+     * @param movieId assigned movie
+     * @param topMovies the top movies and associated scores
+     */
     private String formatCSV(int movieId, MovieScore[] topMovies) {
         DecimalFormat df = new DecimalFormat("0.00");
         String csv = "";
@@ -227,31 +269,10 @@ public class NonPersonalizedRecommender {
 
         NonPersonalizedRecommender npr = new NonPersonalizedRecommender();
         Map<String, String> movies = npr.loadMovies();
-        /*
-        for (String key : movies.keySet()) {
-            System.out.println(key + " - " + movies.get(key));
-        }
-        */
-
         Map<String, String> users = npr.loadUsers();
-        /*
-        for (String key : users.keySet()) {
-            System.out.println(key + " - " + users.get(key));
-        }
-        */
-
         Map<String, Map<String, Float>> ratings = npr.loadRatings();
-        /*
-        for (String movie : ratings.keySet()) {
-            System.out.println(movie);
-            Map<String, Float> ratingsForAMovie = ratings.get(movie);
-            for (String user : ratingsForAMovie.keySet()) {
-                Float rating = ratingsForAMovie.get(user);
-                System.out.println("=> " + user + '\t' + rating);
-            }
-        }
-        */
 
+        // movied IDs used as reference for recommendations
         int[] assignedMovieId = {11, 121, 8587};
 
         // Simple formula
